6c59d8a613944d7c2314add7469aee24b01a9b3e
GiuliaAgostini22 <agostinigiulia22@gmail.com>
2025-03-13 16:31:09 +0100

    prima commit

---
+++ src/main/java/com/example/progetto_jekins/ProgettoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.progetto_jekins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progetto_jekins/ProgettoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progetto_jekins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
